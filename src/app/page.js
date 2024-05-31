b6bc5144c4ee26cbaae13a6a955ac4f2c226959a
Md-nadeeem <nadeem.mohd81423@gmail.com>
2024-05-31 09:48:26 +0530

    table task

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,325 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from 'react';
 
-export default function Home() {
+const Table = ({ header, mainData, perPageLimits }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageData, setPageData] = useState(mainData);
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [search, setSearch] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedColumns, setSelectedColumns] = useState(header.map(col => col.key));
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+
+  // sorting  function 
+  const sorting = (col) => {
+    let sorted;
+    if (sortOrder === "ASC") {
+      sorted = [...pageData].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setSortOrder("DSC");
+    } else {
+      sorted = [...pageData].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setSortOrder("ASC");
+    }
+    setPageData(sorted);
+  };
+  //  seacrh functionality 
+  const filteredData = pageData.filter(item =>
+    selectedColumns.some(key =>
+      item[key].toString().toLowerCase().includes(search.toLowerCase())
+    )
+  );
+
+  // search functionality ending here 
+
+  const perPageData = perPageLimits;
+  const lastIndex = currentPage * perPageData;
+  const firstIndex = lastIndex - perPageData;
+  const records = filteredData.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(filteredData.length / perPageData);
+  const number = [...Array(npage).keys()].map(i => i + 1);
+
+
+  // pagination starting here 
+
+  const NextPage = () => {
+    if (currentPage < npage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const PrePage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const changeCpage = (id) => {
+    setCurrentPage(id);
+  };
+  // pagination ending here 
+
+  
+  //  custom rendering function  for styling 
+  const styleHandling = (row, key) => {
+    let style = {};
+    const age = Number(row.age);
+    if (key === 'age') {
+      if (age > 38) {
+        style.backgroundColor = 'green';
+        style.color = 'yellow';
+      }
+      if (age === 49) {
+        style.fontWeight = 'bold';
+      }
+    }
+    if (key === 'first_name' && age === 49) {
+      style.backgroundColor = 'red';
+    }
+    return style;
+  };
+  // ending here 
+  const FiltereColumns = header.filter(col => selectedColumns.includes(col.key));
+  
+  
+  // left checkbox function 
+
+  const handleSelectAllRows = (e) => {
+    if (e.target.checked) {
+      setSelectedRows(records.map((row) => row.index));
+    } else {
+      setSelectedRows([]);
+    }
+    console.log(selectedRows)
+  };
+
+  const handleRowCheckboxChange = (index) => {
+    console.log("hello",index)
+    setSelectedRows((prevSelectedRows) =>
+    prevSelectedRows.includes(index)
+  
+        ? prevSelectedRows.filter((id) => id !== index)
+        : [...prevSelectedRows, index]
+    );
+   
+  };
+  // ending here left checkbox 
+
+
+//  dropdown  selector  
+  const handleColumnCheckboxChange = (event) => {
+    const columnKey = event.target.value;
+    setSelectedColumns((prevSelectedColumns) =>
+      event.target.checked
+        ? [...prevSelectedColumns, columnKey]
+        : prevSelectedColumns.filter((key) => key !== columnKey)
+    );
+  };
+  
+
+  const renderOption = (col) => (
+    <div key={col.key} className='flex items-center'>
+      <input
+        type='checkbox'
+        id={col.key}
+        value={col.key}
+        checked={selectedColumns.includes(col.key)}
+        onChange={handleColumnCheckboxChange}
+        className='mr-2'
+      />
+      <label htmlFor={col.key}>{col.label}</label>
+    </div>
+  );
+  // end here 
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <style>
+        {`
+          table, tr, th, td {
+            border: 1px solid white;
+            text-align: center;
+            padding:10px;
+          }
+          tr:hover {
+            background-color:yellow;
+            color: black;
+          }
+          ul {
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            padding: 0;
+          }
+          li {
+            margin: 0 5px;
+          }
+          a {
+            cursor: pointer;
+            padding: 5px 10px;
+            border: 1px solid black;
+            border-radius: 5px;
+            text-decoration: none;
+          }
+          a.disabled {
+            pointer-events: none;
+            color: grey;
+            border-color: grey;
+          }
+        `}
+      </style>
+      <div className=' text-left flex justify-center items-center mt-10 '>
+        <div>
+          <button
+            type='button'
+            className='px-2 py-1 bg-black border rounded-md relative '
+            onClick={toggleDropdown}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Column select &nbsp;{isOpen ? '🔼' : "🔽"}
+          </button>
+        </div>
+        {isOpen && (
+          <div className='absolute mt-10 top-1 left-[50px] w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 '>
+            <div className='py-1'>
+              {header.map((col) => (
+                <label key={col.key} className='flex items-center px-4 py-2 text-black'>
+                  {renderOption(col)}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+        <div className=' text-black'>
+          <div>
+            <input
+              placeholder='Search...'
+              onChange={(e) => setSearch(e.target.value)}
+              className='ml-2'
             />
-          </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex justify-center mt-6'>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  checked={selectedRows.length === records.length}
+                  onChange={handleSelectAllRows}
+                />
+              </th>
+              {FiltereColumns.map((column) => (
+          <th key={column.key} onClick={() => column.sorted !== false && sorting(column.key)}>
+            {column.label}
+          </th>
+        ))}
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((row, index) => (
+              <tr key={index}>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedRows.includes(row.index)}
+                    onChange={() => handleRowCheckboxChange(row.index)}
+                  />
+                </td>
+                {FiltereColumns.map((column) => (
+                  <td
+                    key={column.key}
+                    style={styleHandling(row, column.key)}
+                  >
+                    {column.render ? column.render(row[column.key], row) : row[column.key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+      <nav className='mt-10'>
+        <ul>
+          <li>
+            <a href='#' className={currentPage === 1 ? 'disabled' : ''} onClick={PrePage}>Prev</a>
+          </li>
+          {number.map((n) => (
+            <li key={n}>
+              <a href='#' className={currentPage === n ? 'disabled' : ''} onClick={() => changeCpage(n)}>{n}</a>
+            </li>
+          ))}
+          <li>
+            <a href='#' className={currentPage === npage ? 'disabled' : ''} onClick={NextPage}>Next</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Page() {
+  const [perPageLimit, setPerPageLimit] = useState(5);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  function handleLastNameClick(row) {
+    console.log(row);
+    if (row && row.employee_id) {
+      alert(`Employee ID: ${row.employee_id}`);
+    } else {
+      alert("Employee ID not found");
+    }
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  const handlePerPageLimitChange = (event) => {
+    setPerPageLimit(parseInt(event.target.value));
+  };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  const columns = [
+    { label: "ID", key: "index",sorted:false },
+    { label: "First Name", key: "first_name", render: (id, row) => <span onClick={() => handleLastNameClick(row)}>{id}</span> },
+    { label: "Email", key: "email" },
+    { label: "Age", key: "age" },
+    { label: "Employee ID", key: "employee_id", render: (id) => <a href={`#employee/${id}`}>{id}</a> },
+    { label: "Gender", key: "gender", render: (text) => <span style={{ color: text === 'male' ? 'red' : 'blue' }}>{text}</span> },
+  ];
+
+  const data = [
+    { "index": "26", "first_name": "Cheslie", "email": "cclayworthp@lulu.com", "age": "34", "employee_id": "37", "gender": "female" },
+    { "index": "3", "first_name": "Robert", "email": "rhultberg2@wordpress.com", "age": "49", "employee_id": "4", "gender": "male" },
+    { "index": "1", "first_name": "Doralynne", "email": "dlabitt0@disqus.com", "age": "28", "employee_id": "29677", "gender": "female" },
+    { "index": "4", "first_name": "Creighton", "email": "crodriguez3@storify.com", "age": "39", "employee_id": "35770", "gender": "male" },
+    { "index": "6", "first_name": "Shem", "email": "ssaladin5@amazon.co.jp", "age": "22", "employee_id": "32862", "gender": "male" },
+    { "index": "7", "first_name": "Maddi", "email": "mcolnet6@google.fr", "age": "33", "employee_id": "98765", "gender": "female" },
+    { "index": "13", "first_name": "Gilburt", "email": "gthorringtonc@google.ca", "age": "27", "employee_id": "y87bkew", "gender": "male" },
+    { "index": "5", "first_name": "Emalee", "email": "eyendall4@businesswire.com", "age": "30", "employee_id": "bb222", "gender": "male" },
+    { "index": "8", "first_name": "Clarie", "email": "cbrockwell7@reddit.com", "age": "31", "employee_id": "0987", "gender": "female" },
+    { "index": "9", "first_name": "Norrie", "email": "nbraunstein8@fotki.com", "age": "40", "employee_id": "345678", "gender": "male" },
+    { "index": "11", "first_name": "Anthia", "email": "acransona@washingtonpost.com", "age": "29", "employee_id": "345", "gender": "female" },
+    { "index": "12", "first_name": "Casandra", "email": "csarverb@diigo.com", "age": "35", "employee_id": "9877", "gender": "male" },
+    { "index": "14", "first_name": "Angelita", "email": "aaizicd@cpanel.net", "age": "41", "employee_id": "96666", "gender": "female" }
+  ];
+
+  return (
+    <>
+      <Table header={columns} mainData={data} perPageLimits={perPageLimit} />
+      <div className='flex justify-center mt-4'>
+        <label htmlFor="perPageLimit" >Items per page:</label>
+        <select id="perPageLimit" value={perPageLimit} onChange={handlePerPageLimitChange} className='text-black align-middle'>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
       </div>
-    </main>
+    </>
   );
 }
+
+export default Page;
